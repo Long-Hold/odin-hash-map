a9c394c6f9363e2587dd5f1cd0137c2676bcdd81
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-02-19 16:56:43 -0500

    feat: Implement typechecking test for hash method

--- src/tests/hashMap.test.js
+++ src/tests/hashMap.test.js
@@ -6,12 +6,22 @@ describe('class HashMap', () => {
         hashMap = new HashMap();
     });
 
-    describe('getter methods', () => {
+    describe('getters', () => {
         test('loadFactor returns its value', () => {
             expect(hashMap.loadFactor).toBe(0.75);
         });
         test('capacity returns its default size', () => {
             expect(hashMap.capacity).toBe(16);
+        });
+    });
+
+    describe('methods', () => {
+        describe('hash()', () => {
+            test.each(
+                [1, 2.3, {}, [], new String()]
+            )('throws TypeError when passed a non string value: %s', (input) => {
+                expect(() => hashMap.hash(input)).toThrow(TypeError);
+            });
         });
     });
 });
